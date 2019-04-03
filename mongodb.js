9b914cb61a129809e249b4a99e2b2ec3ee5a9227
iamshadmirza <md.shad1@yahoo.com>
2019-04-03 07:38:15 +0530

    understanding ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,24 +1,28 @@
 //CRUD Create Read Update Delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'taskmanager';
+
+const id = new ObjectID();
+console.log('id', id);
+console.log('timestamp', id.getTimestamp());
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error) { return console.log(error); }
     console.log('Connected correctly');
     const db = client.db(databaseName);
-    // db.collection('users').insertOne({
-    //     name: 'Mohd Shad Mirza',
-    //     age: 23
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log(result.ops);
-    // });
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Aditya Kapoor',
+        age: 24
+    }, (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(result.ops);
+    });
 
     // db.collection('users').insertMany([
     //     {
@@ -36,19 +40,19 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     //     console.log(result.ops);
     // });
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Start Learning MongoDB',
-            completed: true,
-        },
-        {
-            description: 'Master MongoDB',
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log(result.ops);
-    });
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Start Learning MongoDB',
+    //         completed: true,
+    //     },
+    //     {
+    //         description: 'Master MongoDB',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     console.log(result.ops);
+    // });
 })
