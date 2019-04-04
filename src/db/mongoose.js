920ba502ff1d8e15941f5a1d611b9a026ae05574
iamshadmirza <md.shad1@yahoo.com>
2019-04-04 07:37:26 +0530

    Intro to mongoose and data model

---
+++ src/db/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+
+const me = new User({
+    name: 'Mohd Shad Mirza',
+    age: 23
+});
+
+me.save().then(() => {
+    console.log(me);
+}).catch((error) => console.log(error));
