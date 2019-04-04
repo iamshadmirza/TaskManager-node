b73d34a72f2d99fb0eddb9c90db305ae022a301f
iamshadmirza <md.shad1@yahoo.com>
2019-04-04 06:40:54 +0530

    refractoring

--- mongodb.js
+++ mongodb.js
@@ -15,103 +15,103 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
     //CREATE
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Aditya Kapoor',
-    //     age: 24
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log(result.ops);
-    // });
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Aditya Kapoor',
+        age: 24
+    }, (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(result.ops);
+    });
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Minhaj Ahmad Khan',
-    //         age: 25,
-    //     },
-    //     {
-    //         name: 'Urooj Ahmad',
-    //         age: 23
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log(result.ops);
-    // });
+    db.collection('users').insertMany([
+        {
+            name: 'Minhaj Ahmad Khan',
+            age: 25,
+        },
+        {
+            name: 'Urooj Ahmad',
+            age: 23
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(result.ops);
+    });
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Start Learning MongoDB',
-    //         completed: true,
-    //     },
-    //     {
-    //         description: 'Master MongoDB',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log(result.ops);
-    // });
+    db.collection('tasks').insertMany([
+        {
+            description: 'Start Learning MongoDB',
+            completed: true,
+        },
+        {
+            description: 'Master MongoDB',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(result.ops);
+    });
 
     //READ
-    // db.collection('users').findOne({ name: 'Mohd Shad Mirza' }, (error, user) => {
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     console.log(user);
-    // });
-    // db.collection('users').findOne({ _id: new ObjectID("5ca411d7effa2205f09667b6") }, (error, user) => {
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     console.log(user);
-    // });
-    // db.collection('users').find({ age: 23 }).toArray((error, users) => {
-    //     console.log(users);
-    // });
-    // db.collection('users').find({ age: 23 }).count((error, count) => {
-    //     console.log(count);
-    // });
+    db.collection('users').findOne({ name: 'Mohd Shad Mirza' }, (error, user) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(user);
+    });
+    db.collection('users').findOne({ _id: new ObjectID("5ca411d7effa2205f09667b6") }, (error, user) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(user);
+    });
+    db.collection('users').find({ age: 23 }).toArray((error, users) => {
+        console.log(users);
+    });
+    db.collection('users').find({ age: 23 }).count((error, count) => {
+        console.log(count);
+    });
 
     //UPDATE
-    // db.collection('users').updateOne({
-    //     _id: new ObjectID("5ca411d7effa2205f09667b5")
-    // }, {
-    //         $set: {
-    //             name: 'Shweta'
-    //         }
-    //     }).then((result) => {
-    //         console.log('result update', result);
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
+    db.collection('users').updateOne({
+        _id: new ObjectID("5ca411d7effa2205f09667b5")
+    }, {
+            $set: {
+                name: 'Shweta'
+            }
+        }).then((result) => {
+            console.log('result update', result);
+        }).catch((error) => {
+            console.log(error);
+        });
 
-    // db.collection('users').updateOne({
-    //     _id: new ObjectID("5ca411d7effa2205f09667b5")
-    // }, {
-    //         $inc: {
-    //             age: -1
-    //         }
-    //     }).then((result) => {
-    //         console.log('result update', result);
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
+    db.collection('users').updateOne({
+        _id: new ObjectID("5ca411d7effa2205f09667b5")
+    }, {
+            $inc: {
+                age: -1
+            }
+        }).then((result) => {
+            console.log('result update', result);
+        }).catch((error) => {
+            console.log(error);
+        });
 
-    // db.collection('tasks').updateMany({
-    //     completed: false
-    // }, {
-    //         $set: {
-    //             completed: true
-    //         }
-    //     }).then((result) => {
-    //         console.log('result update', result);
-    //     }).catch(error => console.log(error));
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+            $set: {
+                completed: true
+            }
+        }).then((result) => {
+            console.log('result update', result);
+        }).catch(error => console.log(error));
 
     //DELETE
     db.collection('users').deleteMany({ age: 23 })
