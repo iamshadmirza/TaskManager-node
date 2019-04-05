c12c0c0b7679f7ae8f135c6a4fedd5e24b518a42
iamshadmirza <md.shad1@yahoo.com>
2019-04-05 07:50:30 +0530

    created api #readuser by id and all

--- src/index.js
+++ src/index.js
@@ -20,6 +20,28 @@ app.post('/users', (req, res) => {
         .catch((error) => res.status(400).send(error));
 });
 
+app.get('/users', (req, res) => {
+    User.find({})
+        .then((users) => {
+            res.send(users);
+        })
+        .catch((error) => {
+            res.status(500).send();
+        })
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send();
+            }
+            res.send(user);
+        })
+        .catch((error) => res.status(500).send(error));
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Tasks(req.body);
     task.save()
