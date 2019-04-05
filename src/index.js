cb22c8350006165b84afd4f639a2ad33925f5f30
iamshadmirza <md.shad1@yahoo.com>
2019-04-05 07:16:05 +0530

    next api #createtask

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 require('./db/mongoose');
 const User = require('./models/users');
+const Tasks = require('./models/tasks');
 
 const port = process.env.PORT || 3000;
 
@@ -18,3 +19,12 @@ app.post('/users', (req, res) => {
         .then(() => { res.status(201).send(user) })
         .catch((error) => res.status(400).send(error));
 });
+
+app.post('/tasks', (req, res) => {
+    const task = new Tasks(req.body);
+    task.save()
+        .then(() => {
+            res.status(201).send(task);
+        })
+        .catch(error => res.status(400).send(error));
+})
