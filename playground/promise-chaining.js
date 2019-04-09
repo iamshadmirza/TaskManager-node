0b610065647c166f0aed3610ff87c7303b2a3dbc
iamshadmirza <md.shad1@yahoo.com>
2019-04-09 08:24:40 +0530

    mistake correction

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -7,3 +7,13 @@ User.findByIdAndUpdate('5ca566030b56d91f7cc3094c', { age: 1 }).then((user) => {
 }).then((result) => {
     console.log(result);
 }).catch(error => console.log(error));
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+}
+
+updateAgeAndCount('5ca566030b56d91f7cc3094c', 2).then(count => {
+    console.log(count);
+}).catch(error => console.log(error));
