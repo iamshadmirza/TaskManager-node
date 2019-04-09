1b1f5c8956397312fa807b296fcd5c6aafc8d478
iamshadmirza <md.shad1@yahoo.com>
2019-04-09 08:22:07 +0530

    then/catch to async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,9 +1,19 @@
 require('../src/db/mongoose');
 var Tasks = require('../src/models/tasks');
 
-Tasks.findByIdAndDelete('5ca567a56ebe290c60857cbf').then((task) => {
-    console.log(task);
-    return Tasks.countDocuments({ completed: false });
-}).then((result) => {
-    console.log(result);
+// Tasks.findByIdAndDelete('5ca567a56ebe290c60857cbf').then((task) => {
+//     console.log(task);
+//     return Tasks.countDocuments({ completed: false });
+// }).then((result) => {
+//     console.log(result);
+// }).catch(error => console.log(error));
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await user.findByIdAndUpdate(id, { age });
+    const count = await user.countDocuments({ age });
+    return count;
+}
+
+updateAgeAndCount('5ca567a56ebe290c60857cbf', 2).then(count => {
+    console.log(count);
 }).catch(error => console.log(error));
