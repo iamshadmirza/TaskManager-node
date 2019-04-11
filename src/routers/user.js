f90a54c2ffddf1318ef7282e5802adc28df4b1c4
iamshadmirza <md.shad1@yahoo.com>
2019-04-11 06:33:06 +0530

    login with credentials

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,6 +13,16 @@ router.post('/users', async (req, res) => {
     }
 });
 
+//login user
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        res.send(user);
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
 //read users
 router.get('/users', async (req, res) => {
     try {
