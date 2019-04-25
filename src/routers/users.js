67f00eab38e5acfb924e6109dfce875773fa6d87
iamshadmirza <md.shad1@yahoo.com>
2019-04-25 16:32:50 +0530

    bug fixes

--- src/routers/users.js
+++ src/routers/users.js
@@ -55,17 +55,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.status(200).send(req.user);
 });
 
-// router.get('/users/:id', async (req, res) => {
-//     const _id = req.params.id;
-//     try {
-//         const user = await User.findById(_id);
-//         if (!user) { return res.status(404).send(); }
-//         res.send(user);
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// });
-
 //update user
 router.patch('/users/me', auth, async (req, res) => {
     const allowedUpdates = ['name', 'email', 'age', 'password'];
