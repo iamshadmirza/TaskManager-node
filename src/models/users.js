823882fbd6fca10afb945b6979e91bcd21363fc9
iamshadmirza <md.shad1@yahoo.com>
2019-06-12 07:53:00 +0530

    image upload auth, delete and get

--- src/models/users.js
+++ src/models/users.js
@@ -42,6 +42,9 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    avatar: {
+        type: Buffer
+    },
     tokens: [{
         token: {
             type: String,
--- src/routers/users.js
+++ src/routers/users.js
@@ -85,7 +85,6 @@ router.delete('/users/me', auth, async (req, res) => {
 
 //upload avatar
 const upload = multer({
-    dest: 'avatar',
     limits: {
         fileSize: 100000
     },
@@ -97,10 +96,31 @@ const upload = multer({
     }
 });  //config
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message });
 });
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+});
+
+router.get('/users/me/avatar', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
+})
 
 module.exports = router;
