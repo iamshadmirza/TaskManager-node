fb42bdc7e59221ff6e15aee1d0bd998028acfb57
iamshadmirza <md.shad1@yahoo.com>
2019-06-19 06:26:48 +0530

    decoupled db setup

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,25 +1,9 @@
 const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/users');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: 'Urooj',
-    email: 'urooj@test.com',
-    age: 23,
-    password: 'BabyMartinSch00l!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
-});
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
     let response = await request(app).post('/users').send({
