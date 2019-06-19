97ba4d562c400d626e9c289d6c64236102478c73
iamshadmirza <md.shad1@yahoo.com>
2019-06-19 06:11:56 +0530

    wrapping up user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -86,3 +86,35 @@ test('Should not delete profile of unauthenticated user', async () => {
         .send()
         .expect(401);
 });
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/driver.png')
+        .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Minhaj'
+        })
+        .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user.name).toEqual('Minhaj');
+});
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'India'
+        })
+        .expect(400);
+});
